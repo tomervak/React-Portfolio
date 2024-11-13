da3def263173ed349bc9f80c9120c330f7d9b963
tomervak <tomervak98@gmail.com>
2024-11-13 22:31:32 +0200

    1

---
+++ reacte-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
+export const Hero=()=>{
+    return (<section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>
+                Hi, I'm Tomer.
+            </h1>
+            <p className={styles.description}>
+            I'm a Software Developer in the final stages of degree completion , B.Sc. in Computer Science
+            Graduating from Ben Gurion University.
+            </p>
+            <a className={styles.contactBtn} href="mailto:tomervak98@gmail.com">contact me</a>
+        </div>
+        <img  className={styles.heroImg} src={getImageUrl("hero/heroImg.png")} alt="hero img" />
+        <div className={styles.topBlur}/>
+        <div className={styles.bottomBlur}/>
+    </section>);
+};
